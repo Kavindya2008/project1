7b0f4498f1fbeea2c2f3c51f14842351519de359
Kavindya2008 <annefilishiyadehoat@gmail.com>
2026-03-17 15:36:39 +0530

    Update lab1.py

---
+++ lab1.py
@@ -0,0 +1,11 @@
+print("kavindya")
+print("st.lawrence's convent")
+#this is a single line comment
+"""This is a multi line comment"""
+#we use comments to understand the code clearly
+print("22 divided by 7 is:", 22/7)
+#Concentration mean joining things together usually strings
+
+operation="sum"
+total=8
+print(operation + "is" + str(total)) 
